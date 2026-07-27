e86a4e488437b5a92bccee47f03ed13de34ee2bc
Arcticoz <bayu90048@gmail.com>
2026-07-27 14:56:24 +0700

    Fix wagmi v3 useBalance breaking change, remove unused portfolio components

--- constants/tokens.ts
+++ constants/tokens.ts
@@ -1,32 +1,18 @@
-import { Token }
-from "@/types/Token";
+import { Token } from "@/types/Token";
 
 export const TOKENS: {
-
   USDC: Token;
-
   EURC: Token;
-
 } = {
-
   USDC: {
-
-    symbol:
-      "USDC",
-
-    address:
-      "0x3600000000000000000000000000000000000000"
-
+    symbol: "USDC",
+    address: "0x3600000000000000000000000000000000000000",
+    decimals: 6,
   },
 
   EURC: {
-
-    symbol:
-      "EURC",
-
-    address:
-      "0x89B50855Aa3bE2F677cD6303Cec089B5F319D72a"
-
-  }
-
+    symbol: "EURC",
+    address: "0x89B50855Aa3bE2F677cD6303Cec089B5F319D72a",
+    decimals: 6,
+  },
 };
--- types/Token.ts
+++ types/Token.ts
@@ -1,7 +1,5 @@
 export type Token = {
-
   symbol: string;
-
   address: `0x${string}`;
-
+  decimals: number;
 };
